c5635f067482d591e9fe509bbc3cbdc0aa3d3266
scloud <scloudrun@gmail.com>
2017-03-02 10:10:13 +0800

    路由配置captcha,调用自身serve

--- routers/router.go
+++ routers/router.go
@@ -3,11 +3,12 @@ package routers
 import (
 	"beego-captcha/controllers"
 	"github.com/astaxie/beego"
+	"github.com/dchest/captcha"
 )
 
 func init() {
 	beego.Router("/", &controllers.MainController{}, "get:Login")
-	beego.Router("/home", &controllers.MainController{}, "get:Login")
-	beego.Router("/captcha", &controllers.CaptchaController{}, "get:GetCaptcha")
+	beego.Router("/home", &controllers.MainController{}, "get:Home")
 	beego.Router("/verify", &controllers.CaptchaController{}, "get:VerifyCaptcha")
+	beego.Handler("/captcha/*.png", captcha.Server(240, 80))
 }
